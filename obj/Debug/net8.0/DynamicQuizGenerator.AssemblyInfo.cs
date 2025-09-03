7a1e4dd20c9c40be828d706c398004abe4b68142
Manoj Shrestha <manoj.shrestha2081@gmail.com>
2025-09-03 16:54:15 +0545

    UIs Updated

--- obj/Debug/net8.0/DynamicQuizGenerator.AssemblyInfo.cs
+++ obj/Debug/net8.0/DynamicQuizGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DynamicQuizGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5301e4398b38101f8678ece9b235b2d4aa9836e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6cd9cb2dee7c1eec0a4e48362d4a935de25af1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DynamicQuizGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DynamicQuizGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
